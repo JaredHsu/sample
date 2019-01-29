be277b9d954872f57bc12dc3f714db618253c6c5
jared <thukyhsu@qq.com>
2019-01-29 04:03:01 +0000

    Finish layout adn routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('title', 'Sample') - Laravel 新手入门教程 </title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+@include('layouts._header')
+
+<div class="container">
 	@yield('content')
+</div>
+
+@include('layouts._footer')	
 </body>
 </html>

f934d36b02577f8fafa6a612b77e84939c5f43b1
jared <thukyhsu@qq.com>
2019-02-13 12:31:32 +0000

    dd user following

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // 
-        //Model::unguard();
+        Model::unguard();
 
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
 
-        //Model::reguard();
+        Model::reguard();
     }
 }
